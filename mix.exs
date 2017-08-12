2375eea92e1e4c398fa091fafd2c92c7a0245dee
Cameron Bytheway <bytheway.cameron@gmail.com>
2017-08-12 11:58:37 -0600

    release 0.1.3

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule MimetypeParser.Mixfile do
 
   def project do
     [app: :mimetype_parser,
-     version: "0.1.2",
+     version: "0.1.3",
      elixir: "~> 1.0",
      description: "parse mimetypes",
      package: package(),
